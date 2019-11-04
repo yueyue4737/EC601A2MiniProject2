4348b678c0be5e1404959045b77a0224191e901c
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-04 11:07:35 -0500

    Update 2spam.py

--- Intro_Snorkel/2spam.py
+++ Intro_Snorkel/2spam.py
@@ -626,3 +626,244 @@ acc_original = train_and_test(X_train, Y_train)
 
 print(f"Test Accuracy (original training data): {100 * acc_original:.1f}%")
 print(f"Test Accuracy (augmented training data): {100 * acc_augmented:.1f}%")
+# step3: slicing
+# # ✂️ Snorkel Intro Tutorial: _Data Slicing_
+# In real-world applications, some model outcomes are often more important than others — e.g. vulnerable cyclist detections in an autonomous driving task, or, in our running **spam** application, potentially malicious link redirects to external websites.
+# Traditional machine learning systems optimize for overall quality, which may be too coarse-grained.
+# Models that achieve high overall performance might produce unacceptable failure rates on critical slices of the data — data subsets that might correspond to vulnerable cyclist detection in an autonomous driving task, or in our running spam detection application, external links to potentially malicious websites.
+#
+# In this tutorial, we:
+# 1. **Introduce _Slicing Functions (SFs)_** as a programming interface
+# 1. **Monitor** application-critical data subsets
+# 2. **Improve model performance** on slices
+
+# %% [markdown] {"tags": ["md-exclude"]}
+# First, we'll set up our notebook for reproducibility and proper logging.
+
+import logging
+import os
+from snorkel.utils import set_seed
+
+# For reproducibility
+os.environ["PYTHONHASHSEED"] = "0"
+set_seed(111)
+
+# Make sure we're running from the spam/ directory
+if os.path.basename(os.getcwd()) == "snorkel-tutorials":
+    os.chdir("spam")
+
+# To visualize logs
+logger = logging.getLogger()
+logger.setLevel(logging.WARNING)
+
+import pandas as pd
+
+DISPLAY_ALL_TEXT = False
+
+pd.set_option("display.max_colwidth", 0 if DISPLAY_ALL_TEXT else 50)
+
+# SFs are intended to be used *after the training set has already been labeled* by LFs (or by hand) in the training data pipeline.
+
+from utils import load_spam_dataset
+
+df_train, df_valid, df_test = load_spam_dataset(load_train_labels=True, split_dev=False)
+
+# %% [markdown]
+# ## 1. Write slicing functions
+# We leverage *slicing functions* (SFs), which output binary _masks_ indicating whether an data point is in the slice or not.
+# Each slice represents some noisily-defined subset of the data (corresponding to an SF) that we'd like to programmatically monitor.
+
+import re
+from snorkel.slicing import slicing_function
+
+@slicing_function()
+def short_link(x):
+    """Returns whether text matches common pattern for shortened ".ly" links."""
+    return bool(re.search(r"\w+\.ly", x.text))
+
+sfs = [short_link]
+
+# ### Visualize slices
+
+from snorkel.slicing import slice_dataframe
+
+short_link_df = slice_dataframe(df_valid, short_link)
+
+short_link_df[["text", "label"]]
+
+# ## 2. Monitor slice performance with [`Scorer.score_slices`](https://snorkel.readthedocs.io/en/master/packages/_autosummary/analysis/snorkel.analysis.Scorer.html#snorkel.analysis.Scorer.score_slices)
+# In this section, we'll demonstrate how we might monitor slice performance on the `short_link` slice — this approach is compatible with _any modeling framework_.
+
+# ### Train a simple classifier
+# First, we featurize the data — as you saw in the introductory Spam tutorial, we can extract simple bag-of-words features and store them as numpy arrays.
+
+from sklearn.feature_extraction.text import CountVectorizer
+from utils import df_to_features
+
+vectorizer = CountVectorizer(ngram_range=(1, 1))
+X_train, Y_train = df_to_features(vectorizer, df_train, "train")
+X_valid, Y_valid = df_to_features(vectorizer, df_valid, "valid")
+X_test, Y_test = df_to_features(vectorizer, df_test, "test")
+
+# We define a `LogisticRegression` model from `sklearn` and show how we might visualize these slice-specific scores.
+
+from sklearn.linear_model import LogisticRegression
+
+sklearn_model = LogisticRegression(C=0.001, solver="liblinear")
+sklearn_model.fit(X=X_train, y=Y_train)
+print(f"Test set accuracy: {100 * sklearn_model.score(X_test, Y_test):.1f}%")
+
+from snorkel.utils import preds_to_probs
+
+preds_test = sklearn_model.predict(X_test)
+probs_test = preds_to_probs(preds_test, 2)
+
+from snorkel.slicing import PandasSFApplier
+
+applier = PandasSFApplier(sfs)
+S_test = applier.apply(df_test)
+
+from snorkel.analysis import Scorer
+
+scorer = Scorer(metrics=["accuracy", "f1"])
+
+scorer.score_slices(
+    S=S_test, golds=Y_test, preds=preds_test, probs=probs_test, as_dataframe=True
+)
+
+# ### Write additional slicing functions (SFs)
+# Slices are dynamic — as monitoring needs grow or change with new data distributions or application needs, an ML pipeline might require dozens, or even hundreds, of slices.
+
+from snorkel.slicing import SlicingFunction, slicing_function
+from snorkel.preprocess import preprocessor
+
+# Keyword-based SFs
+def keyword_lookup(x, keywords):
+    return any(word in x.text.lower() for word in keywords)
+
+def make_keyword_sf(keywords):
+    return SlicingFunction(
+        name=f"keyword_{keywords[0]}",
+        f=keyword_lookup,
+        resources=dict(keywords=keywords),
+    )
+
+keyword_subscribe = make_keyword_sf(keywords=["subscribe"])
+keyword_please = make_keyword_sf(keywords=["please", "plz"])
+
+# Regex-based SF
+@slicing_function()
+def regex_check_out(x):
+    return bool(re.search(r"check.*out", x.text, flags=re.I))
+
+# Heuristic-based SF
+@slicing_function()
+def short_comment(x):
+    """Ham comments are often short, such as 'cool video!'"""
+    return len(x.text.split()) < 5
+
+# Leverage preprocessor in SF
+from textblob import TextBlob
+
+@preprocessor(memoize=True)
+def textblob_sentiment(x):
+    scores = TextBlob(x.text)
+    x.polarity = scores.sentiment.polarity
+    return x
+
+@slicing_function(pre=[textblob_sentiment])
+def textblob_polarity(x):
+    return x.polarity > 0.9
+
+# Again, we'd like to visualize data points in a particular slice. This time, we'll inspect the `textblob_polarity` slice.
+# Most data points with high-polarity sentiments are strong opinions about the video — hence, they are usually relevant to the video, and the corresponding labels are $0$.
+# We might define a slice here for *product and marketing reasons*, it's important to make sure that we don't misclassify very positive comments from good users.
+
+polarity_df = slice_dataframe(df_valid, textblob_polarity)
+
+polarity_df[["text", "label"]].head()
+
+extra_sfs = [
+    keyword_subscribe,
+    keyword_please,
+    regex_check_out,
+    short_comment,
+    textblob_polarity,
+]
+
+sfs = [short_link] + extra_sfs
+slice_names = [sf.name for sf in sfs]
+
+applier = PandasSFApplier(sfs)
+S_test = applier.apply(df_test)
+
+scorer.score_slices(
+    S=S_test, golds=Y_test, preds=preds_test, probs=probs_test, as_dataframe=True
+)
+
+# ## 3. Improve slice performance
+# In the following section, we demonstrate a modeling approach that we call _Slice-based Learning,_ which improves performance by adding extra slice-specific representational capacity to whichever model we're using.
+
+# ### Set up modeling pipeline with [`SlicingClassifier`](https://snorkel.readthedocs.io/en/master/packages/_autosummary/slicing/snorkel.slicing.SlicingClassifier.html)
+
+# First, we initialize a dataloaders for each split.
+
+from utils import create_dict_dataloader
+
+BATCH_SIZE = 64
+
+train_dl = create_dict_dataloader(
+    X_train, Y_train, "train", batch_size=BATCH_SIZE, shuffle=True
+)
+valid_dl = create_dict_dataloader(
+    X_valid, Y_valid, "valid", batch_size=BATCH_SIZE, shuffle=False
+)
+test_dl = create_dict_dataloader(
+    X_test, Y_test, "test", batch_size=BATCH_SIZE, shuffle=True
+)
+
+# We'll now initialize a [`SlicingClassifier`](https://snorkel.readthedocs.io/en/master/packages/_autosummary/slicing/snorkel.slicing.SlicingClassifier.html):
+# * `base_architecture`: We define a simple Multi-Layer Perceptron (MLP) in Pytorch to serve as the primary representation architecture. We note that the `BinarySlicingClassifier` is **agnostic to the base architecture** — you might leverage a Transformer model for text, or a ResNet for images.
+# * `head_dim`: identifies the final output feature dimension of the `base_architecture`
+# * `slice_names`: Specify the slices that we plan to train on with this classifier.
+
+from snorkel.slicing import SlicingClassifier
+from utils import get_pytorch_mlp
+
+# Define model architecture
+bow_dim = X_train.shape[1]
+hidden_dim = bow_dim
+mlp = get_pytorch_mlp(hidden_dim=hidden_dim, num_layers=2)
+
+# Init slice model
+slice_model = SlicingClassifier(
+    base_architecture=mlp, head_dim=hidden_dim, slice_names=[sf.name for sf in sfs]
+)
+
+from snorkel.classification import Trainer
+
+trainer = Trainer(lr=1e-4, n_epochs=2)
+trainer.fit(slice_model, [train_dl, valid_dl])
+
+applier = PandasSFApplier(sfs)
+S_train = applier.apply(df_train)
+S_valid = applier.apply(df_valid)
+
+train_dl_slice = slice_model.make_slice_dataloader(
+    train_dl.dataset, S_train, shuffle=True, batch_size=BATCH_SIZE
+)
+valid_dl_slice = slice_model.make_slice_dataloader(
+    valid_dl.dataset, S_valid, shuffle=False, batch_size=BATCH_SIZE
+)
+test_dl_slice = slice_model.make_slice_dataloader(
+    test_dl.dataset, S_test, shuffle=False, batch_size=BATCH_SIZE
+)
+
+# We train a single model initialized with all slice tasks.
+
+from snorkel.classification import Trainer
+
+trainer = Trainer(n_epochs=2, lr=1e-4, progress_bar=True)
+trainer.fit(slice_model, [train_dl_slice, valid_dl_slice])
+
+slice_model.score_slices([valid_dl_slice, test_dl_slice], as_dataframe=True)
