9078576036f1378ae8059f0e2591f987d7561e0f
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-04 10:48:31 -0500

    Update 2spam.py

--- Intro_Snorkel/2spam.py
+++ Intro_Snorkel/2spam.py
@@ -1,1 +1,376 @@
-ff
+# step1: overview tutorial
+# Our goal is to train a classifier over the comment data that can predict whether a comment is spam or not spam.
+# We have access to a large amount of *unlabeled data* in the form of YouTube comments with some metadata.
+# In order to train a classifier, we need to label our data, but doing so by hand for real world applications can be prohibitively slow and expensive, often taking person-weeks or months.
+
+# The tutorial is divided into four parts:
+# 1. **Loading Data**: We load a [YouTube comments dataset](http://www.dt.fee.unicamp.br/~tiago//youtubespamcollection/), originally introduced in ["TubeSpam: Comment Spam Filtering on YouTube"](https://ieeexplore.ieee.org/document/7424299/), ICMLA'15 (T.C. Alberto, J.V. Lochter, J.V. Almeida).
+#
+# 2. **Writing Labeling Functions**: We write Python programs that take as input a data point and assign labels (or abstain) using heuristics, pattern matching, and third-party models.
+#
+# 3. **Combining Labeling Function Outputs with the Label Model**: We use the outputs of the labeling functions over the training set as input to the label model, which assigns probabilistic labels to the training set.
+#
+# 4. **Training a Classifier**: We train a classifier that can predict labels for *any* YouTube comment (not just the ones labeled by the labeling functions) using the probabilistic training labels from step 3.
+
+import os
+
+if os.path.basename(os.getcwd()) == "snorkel-tutorials":
+    os.chdir("spam")
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+os.environ["PYTHONHASHSEED"] = "0"
+
+import pandas as pd
+
+DISPLAY_ALL_TEXT = False
+pd.set_option("display.max_colwidth", 0 if DISPLAY_ALL_TEXT else 50)
+
+from utils import load_spam_dataset
+
+df_train, df_dev, df_valid, df_test = load_spam_dataset()
+
+Y_dev = df_dev.label.values
+Y_valid = df_valid.label.values
+Y_test = df_test.label.values
+
+df_dev.sample(5, random_state=3)
+
+ABSTAIN = -1
+HAM = 0
+SPAM = 1
+
+print(f"Dev SPAM frequency: {100 * (df_dev.label.values == SPAM).mean():.1f}%")
+
+# ## 2. Writing Labeling Functions (LFs)
+
+df_train[["author", "text", "video"]].sample(20, random_state=2)
+
+from snorkel.labeling import labeling_function
+
+@labeling_function()
+def check(x):
+    return SPAM if "check" in x.text.lower() else ABSTAIN
+
+@labeling_function()
+def check_out(x):
+    return SPAM if "check out" in x.text.lower() else ABSTAIN
+
+from snorkel.labeling import PandasLFApplier
+
+lfs = [check_out, check]
+
+applier = PandasLFApplier(lfs=lfs)
+L_train = applier.apply(df=df_train)
+L_dev = applier.apply(df=df_dev)
+
+L_train
+
+coverage_check_out, coverage_check = (L_train != ABSTAIN).mean(axis=0)
+print(f"check_out coverage: {coverage_check_out * 100:.1f}%")
+print(f"check coverage: {coverage_check * 100:.1f}%")
+
+from snorkel.labeling import LFAnalysis
+
+LFAnalysis(L=L_train, lfs=lfs).lf_summary()
+
+LFAnalysis(L=L_dev, lfs=lfs).lf_summary(Y=Y_dev)
+
+from snorkel.analysis import get_label_buckets
+
+buckets = get_label_buckets(Y_dev, L_dev[:, 1])
+df_dev.iloc[buckets[(HAM, SPAM)]]
+
+df_train.iloc[L_train[:, 1] == SPAM].sample(10, random_state=1)
+
+buckets = get_label_buckets(L_train[:, 0], L_train[:, 1])
+df_train.iloc[buckets[(ABSTAIN, SPAM)]].sample(10, random_state=1)
+
+import re
+
+@labeling_function()
+def regex_check_out(x):
+    return SPAM if re.search(r"check.*out", x.text, flags=re.I) else ABSTAIN
+
+lfs = [check_out, check, regex_check_out]
+
+applier = PandasLFApplier(lfs=lfs)
+L_train = applier.apply(df=df_train)
+L_dev = applier.apply(df=df_dev)
+
+LFAnalysis(L=L_train, lfs=lfs).lf_summary()
+
+LFAnalysis(L_dev, lfs).lf_summary(Y=Y_dev)
+
+buckets = get_label_buckets(L_dev[:, 1], L_dev[:, 2])
+df_dev.iloc[buckets[(SPAM, ABSTAIN)]]
+
+buckets = get_label_buckets(L_train[:, 1], L_train[:, 2])
+df_train.iloc[buckets[(SPAM, ABSTAIN)]].sample(10, random_state=1)
+
+from snorkel.preprocess import preprocessor
+from textblob import TextBlob
+
+@preprocessor(memoize=True)
+def textblob_sentiment(x):
+    scores = TextBlob(x.text)
+    x.polarity = scores.sentiment.polarity
+    x.subjectivity = scores.sentiment.subjectivity
+    return x
+
+import matplotlib.pyplot as plt
+
+spam_polarities = [
+    textblob_sentiment(x).polarity for _, x in df_dev.iterrows() if x.label == SPAM
+]
+
+ham_polarities = [
+    textblob_sentiment(x).polarity for _, x in df_dev.iterrows() if x.label == HAM
+]
+
+plt.hist([spam_polarities, ham_polarities])
+plt.title("TextBlob sentiment polarity scores")
+plt.xlabel("Sentiment polarity score")
+plt.ylabel("Count")
+plt.legend(["Spam", "Ham"])
+plt.show()
+
+@labeling_function(pre=[textblob_sentiment])
+def textblob_polarity(x):
+    return HAM if x.polarity > 0.9 else ABSTAIN
+
+spam_subjectivities = [
+    textblob_sentiment(x).subjectivity for _, x in df_dev.iterrows() if x.label == SPAM
+]
+
+ham_subjectivities = [
+    textblob_sentiment(x).subjectivity for _, x in df_dev.iterrows() if x.label == HAM
+]
+
+plt.hist([spam_subjectivities, ham_subjectivities])
+plt.title("TextBlob sentiment subjectivity scores")
+plt.xlabel("Sentiment subjectivity score")
+plt.ylabel("Count")
+plt.legend(["Spam", "Ham"])
+plt.show()
+
+plt.hist([spam_subjectivities, ham_subjectivities], bins=[0, 0.5, 1])
+plt.title("TextBlob sentiment subjectivity scores")
+plt.xlabel("Sentiment subjectivity score")
+plt.ylabel("Count")
+plt.legend(["Spam", "Ham"])
+plt.show()
+
+@labeling_function(pre=[textblob_sentiment])
+def textblob_subjectivity(x):
+    return HAM if x.subjectivity >= 0.5 else ABSTAIN
+
+lfs = [textblob_polarity, textblob_subjectivity]
+
+applier = PandasLFApplier(lfs)
+L_train = applier.apply(df_train)
+L_dev = applier.apply(df_dev)
+
+LFAnalysis(L_train, lfs).lf_summary()
+
+LFAnalysis(L_dev, lfs).lf_summary(Y=Y_dev)
+
+import LabelingFunction
+
+
+def keyword_lookup(x, keywords, label):
+    if any(word in x.text.lower() for word in keywords):
+        return label
+    return ABSTAIN
+
+
+def make_keyword_lf(keywords, label=SPAM):
+    return LabelingFunction(
+        name=f"keyword_{keywords[0]}",
+        f=keyword_lookup,
+        resources=dict(keywords=keywords, label=label),
+    )
+
+keyword_my = make_keyword_lf(keywords=["my"])
+
+keyword_subscribe = make_keyword_lf(keywords=["subscribe"])
+
+keyword_link = make_keyword_lf(keywords=["http"])
+
+keyword_please = make_keyword_lf(keywords=["please", "plz"])
+
+keyword_song = make_keyword_lf(keywords=["song"], label=HAM)
+
+@labeling_function()
+def short_comment(x):
+    """Ham comments are often short, such as 'cool video!'"""
+    return HAM if len(x.text.split()) < 5 else ABSTAIN
+
+from snorkel.preprocess.nlp import SpacyPreprocessor
+
+spacy = SpacyPreprocessor(text_field="text", doc_field="doc", memoize=True)
+
+@labeling_function(pre=[spacy])
+def has_person(x):
+    """Ham comments mention specific people and are short."""
+    if len(x.doc) < 20 and any([ent.label_ == "PERSON" for ent in x.doc.ents]):
+        return HAM
+    else:
+        return ABSTAIN
+
+from snorkel.labeling.lf.nlp import nlp_labeling_function
+
+@nlp_labeling_function()
+def has_person_nlp(x):
+    """Ham comments mention specific people and are short."""
+    if len(x.doc) < 20 and any([ent.label_ == "PERSON" for ent in x.doc.ents]):
+        return HAM
+    else:
+        return ABSTAIN
+
+lfs = [
+    keyword_my,
+    keyword_subscribe,
+    keyword_link,
+    keyword_please,
+    keyword_song,
+    regex_check_out,
+    short_comment,
+    has_person_nlp,
+    textblob_polarity,
+    textblob_subjectivity,
+]
+
+applier = PandasLFApplier(lfs=lfs)
+L_train = applier.apply(df=df_train)
+L_dev = applier.apply(df=df_dev)
+L_valid = applier.apply(df=df_valid)
+
+LFAnalysis(L=L_dev, lfs=lfs).lf_summary(Y=Y_dev)
+
+def plot_label_frequency(L):
+    plt.hist((L != ABSTAIN).sum(axis=1), density=True, bins=range(L.shape[1]))
+    plt.xlabel("Number of labels")
+    plt.ylabel("Fraction of dataset")
+    plt.show()
+
+
+plot_label_frequency(L_train)
+
+from snorkel.labeling import MajorityLabelVoter
+
+majority_model = MajorityLabelVoter()
+preds_train = majority_model.predict(L=L_train)
+
+preds_train
+
+from snorkel.labeling import LabelModel
+
+label_model = LabelModel(cardinality=2, verbose=True)
+label_model.fit(L_train=L_train, n_epochs=1000, lr=0.001, log_freq=100, seed=123)
+
+majority_acc = majority_model.score(L=L_valid, Y=Y_valid)["accuracy"]
+print(f"{'Majority Vote Accuracy:':<25} {majority_acc * 100:.1f}%")
+
+label_model_acc = label_model.score(L=L_valid, Y=Y_valid)["accuracy"]
+print(f"{'Label Model Accuracy:':<25} {label_model_acc * 100:.1f}%")
+
+probs_dev = majority_model.predict_proba(L=L_dev)
+preds_dev = probs_dev >= 0.5
+buckets = get_label_buckets(Y_dev, preds_dev[:, 1])
+
+df_fn_dev = df_dev[["text", "label"]].iloc[buckets[(SPAM, HAM)]]
+df_fn_dev["probability"] = probs_dev[buckets[(SPAM, HAM)], 1]
+
+df_fn_dev.sample(5, random_state=3)
+
+def plot_probabilities_histogram(Y):
+    plt.hist(Y, bins=10)
+    plt.xlabel("Probability of SPAM")
+    plt.ylabel("Number of data points")
+    plt.show()
+
+
+probs_train = label_model.predict_proba(L=L_train)
+plot_probabilities_histogram(probs_train[:, SPAM])
+
+from snorkel.labeling import filter_unlabeled_dataframe
+
+df_train_filtered, probs_train_filtered = filter_unlabeled_dataframe(
+    X=df_train, y=probs_train, L=L_train
+)
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer(ngram_range=(1, 2))
+X_train = vectorizer.fit_transform(df_train_filtered.text.tolist())
+
+X_dev = vectorizer.transform(df_dev.text.tolist())
+X_valid = vectorizer.transform(df_valid.text.tolist())
+X_test = vectorizer.transform(df_test.text.tolist())
+
+import random
+
+import numpy as np
+import tensorflow as tf
+
+
+seed = 1
+np.random.seed(seed)
+random.seed(seed)
+
+session_conf = tf.compat.v1.ConfigProto(
+    intra_op_parallelism_threads=1, inter_op_parallelism_threads=1
+)
+
+from tensorflow.keras import backend as K
+
+tf.set_random_seed(seed)
+sess = tf.compat.v1.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+from snorkel.analysis import metric_score
+from snorkel.utils import preds_to_probs
+from utils import get_keras_logreg, get_keras_early_stopping
+
+keras_model = get_keras_logreg(input_dim=X_train.shape[1])
+
+keras_model.fit(
+    x=X_train,
+    y=probs_train_filtered,
+    validation_data=(X_valid, preds_to_probs(Y_valid, 2)),
+    callbacks=[get_keras_early_stopping()],
+    epochs=20,
+    verbose=0,
+)
+
+preds_test = keras_model.predict(x=X_test).argmax(axis=1)
+test_acc = metric_score(golds=Y_test, preds=preds_test, metric="accuracy")
+print(f"Test Accuracy: {test_acc * 100:.1f}%")
+
+keras_dev_model = get_keras_logreg(input_dim=X_train.shape[1], output_dim=1)
+
+keras_dev_model.fit(
+    x=X_dev,
+    y=Y_dev,
+    validation_data=(X_valid, Y_valid),
+    callbacks=[get_keras_early_stopping()],
+    epochs=20,
+    verbose=0,
+)
+
+preds_test_dev = np.round(keras_dev_model.predict(x=X_test))
+test_acc = metric_score(golds=Y_test, preds=preds_test_dev, metric="accuracy")
+print(f"Test Accuracy: {test_acc * 100:.1f}%")
+
+from snorkel.utils import probs_to_preds
+
+preds_train_filtered = probs_to_preds(probs=probs_train_filtered)
+
+from sklearn.linear_model import LogisticRegression
+
+sklearn_model = LogisticRegression(C=0.001, solver="liblinear")
+sklearn_model.fit(X=X_train, y=preds_train_filtered)
+
+print(f"Test Accuracy: {sklearn_model.score(X=X_test, y=Y_test) * 100:.1f}%")
+
